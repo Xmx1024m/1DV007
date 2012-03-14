35ae9588f5efa7bfb3c25847879a2465c12b4f96
Questo <mathiasandreasen91@gmail.com>
2012-03-14 10:44:36 +0100

    Labb3.uppg2 KLAR

--- src/lab3/uppg2/SearchSortMain.java
+++ src/lab3/uppg2/SearchSortMain.java
@@ -7,20 +7,41 @@ package lab3.uppg2;
 public class SearchSortMain {
 	
 	public static void main(String[] args) {
-		// TODO Test
+		// Demostrerar vår SearSort-klass
 		
 		Comparable[] tal = new Comparable[5];
 		
+		// Lista att sortera
 		tal[0] = 3;
 		tal[1] = 9;
 		tal[2] = 6;
 		tal[3] = 1;
 		tal[4] = 2;
 		
-		SearchSort.selectionSort(tal);
+		// Följande två IF-satser kommer aldrig visas då listan ej ännu är sorterad
+		if (SearchSort.isSorted(tal))
+			System.out.println("Kommer inte visas, listan är inte sorterad");
+		
+		if (SearchSort.binarySearch(tal, 6))
+			System.out.println("Kommer inte visas, listan är inte sorterad");
+		
+		// Fancy utskrivning
+		for (Comparable n : tal)
+			System.out.print(n);
+		
+		System.out.print(" --> ");
+		
+		SearchSort.selectionSort(tal);	// Här sorteras listan
 		
 		for (Comparable n : tal)
 			System.out.print(n);
+		
+		// Följande två IF-satser kommer visas, för nu är listan sorterad
+		if (SearchSort.isSorted(tal))
+			System.out.println("\nListan är nu sorterad från minst -> högst");
+		
+		if (SearchSort.binarySearch(tal, 6))
+			System.out.println("Talet 6 finns i listan");
 	}
 
 }
