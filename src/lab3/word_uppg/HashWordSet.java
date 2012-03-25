253485c98566b37b895719cd324c24bd2288da09
Mathias <Mathias@Mathias-PC>
2012-03-25 14:43:03 +0200

    la till resten

---
+++ src/lab3/word_uppg/HashWordSet.java
@@ -0,0 +1,31 @@
+package lab3.word_uppg;
+
+import java.util.Iterator;
+
+public class HashWordSet implements WordSet {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<Word> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(Word word) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Word word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/lab3/word_uppg/WordSet.java
@@ -0,0 +1,10 @@
+package lab3.word_uppg;
+
+public interface WordSet extends Iterable<Word> {
+	
+	public void add(Word word); // Add word if not already added
+	public boolean contains (Word word); // Return true if word contained
+	public int size (); // Return current set size
+	public String toString (); // Print contained words
+
+}
